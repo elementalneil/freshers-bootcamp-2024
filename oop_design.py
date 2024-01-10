efca030b011a2c92ef1b842bf1934f1d28b74171
Sankhanil Nayek <nayek.sankhanil@gmail.com>
2024-01-10 15:18:04 +0530

    Update oop_design.py

--- oop_design.py
+++ oop_design.py
@@ -9,7 +9,30 @@ class ConsoleDisplayController:
     print(self.__content)
 
 
-def main():
-  display_controller = ConsoleDisplayController()
-  display_controller.set_content('Hello World')
-  display_controller.display()
+class StartsWithStrategy:
+  def __init__(self):
+    self.__starts_with = ""
+
+  def set_starts_with(self, key):
+    self.__starts_with = key
+
+  def invoke(self, item):
+    if item.startswith(self.__starts_with):
+      return True
+    else
+      return False
+
+
+class StringListFilterController:
+  def __init__(self):
+    self.__strategy = StartsWithStrategy()
+
+  def filter(self, string_list):
+    self.__strategy.set_starts_with('AB')
+    filtered_list = []
+
+    for string in string_list:
+      if self.__strategy.invoke(string):
+        filtered_list.append(string)
+
+    return filtered_list
