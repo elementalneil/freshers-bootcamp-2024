ca4070f764a540cdcc8bca54cbba40c35cd3e102
Sankhanil Nayek <nayek.sankhanil@gmail.com>
2024-01-10 12:02:10 +0530

    Updated string_filtering_oops.py

--- string_filtering_oops.py
+++ string_filtering_oops.py
@@ -33,28 +33,28 @@ class Filtering_criteria:
             
 class Filter_functionality_tester:
     def __init__(self):
-        self.search_strings = []
-        self.key = ''
+        self.__search_strings = []
+        self.__key = ''
     
     def take_input(self):
         input_size = int(input('Enter Input Size: '))
         print('Enter String: ')
         for i in range(input_size):
             search_string_input = input()
-            self.search_strings.append(search_string_input)
+            self.__search_strings.append(search_string_input)
             
-        self.key = input('Enter Key: ')
+        self.__key = input('Enter Key: ')
         
     def invoke_string_evaluator(self):
         string_evaluator_obj = String_evaluator()
         criterion_obj = Filtering_criteria()
         
         print('Checking from Start:', string_evaluator_obj.
-                                    filter_(self.search_strings, self.key, criterion_obj.check_from_start))
+                                    filter_(self.__search_strings, self.__key, criterion_obj.check_from_start))
         print('Checking from End:', string_evaluator_obj.
-                                    filter_(self.search_strings, self.key, criterion_obj.check_from_end))
+                                    filter_(self.__search_strings, self.__key, criterion_obj.check_from_end))
         print('Checking for Exact Match:', string_evaluator_obj.
-                                    filter_(self.search_strings, self.key, criterion_obj.check_equal))
+                                    filter_(self.__search_strings, self.__key, criterion_obj.check_equal))
             
             
 def main():
