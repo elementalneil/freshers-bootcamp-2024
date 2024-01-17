dea2dfb34e8988691cac820b43f1ed784f410cb4
Sankhanil Nayek <nayek.sankhanil@gmail.com>
2024-01-17 09:47:53 +0530

    Added Day 7 Notes and Assignment

---
+++ Day 7 Files/Day 7 Assignment/ProductManager.cs
@@ -0,0 +1,214 @@
+using System;
+
+class Product
+{
+    private int productID;
+    private string name;
+    private string manufacturingDate;
+    private int warrantyMonths;
+    private float price;
+    private int stock;
+    private int gst;
+    private int discount;
+
+    // Getters and Setters for private members
+    public int ProductID
+    {
+        get { return productID; }
+        set
+        {
+            if (value > 0)
+                productID = value;
+            else
+                throw new ArgumentException("ProductID should be greater than 0.");
+        }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (value.Length > 3)
+                name = value;
+            else
+                throw new ArgumentException("Name should have a length greater than 3.");
+        }
+    }
+
+    public string ManufacturingDate
+    {
+        get { return manufacturingDate; }
+        set
+        {
+            DateTime currentDate = DateTime.Now;
+            if (DateTime.TryParse(value, out DateTime date) && date < currentDate)
+                manufacturingDate = value;
+            else
+                throw new ArgumentException("Invalid manufacturing date.");
+        }
+    }
+
+    public int Warranty
+    {
+        get { return warrantyMonths; }
+        set
+        {
+            if (value >= 0)
+                warrantyMonths = value;
+            else
+                throw new ArgumentException("Warranty cannot be negative.");
+        }
+    }
+
+    public float Price
+    {
+        get { return price; }
+        set
+        {
+            if (value > 0)
+                price = value;
+            else
+                throw new ArgumentException("Price should be greater than 0.");
+        }
+    }
+
+    public int Stock
+    {
+        get { return stock; }
+        set
+        {
+            if (value > 0)
+                stock = value;
+            else
+                throw new ArgumentException("Stock should be greater than 0.");
+        }
+    }
+
+    public int GST
+    {
+        get { return gst; }
+        set
+        {
+            if (value == 5 || value == 12 || value == 18 || value == 28)
+                gst = value;
+            else
+                throw new ArgumentException("GST should be 5, 12, 18, or 28.");
+        }
+    }
+
+    public int Discount
+    {
+        get { return discount; }
+        set
+        {
+            if (value >= 1 && value <= 30)
+                discount = value;
+            else
+                throw new ArgumentException("Discount should be between 1 and 30.");
+        }
+    }
+
+    // Public methods
+    public float GetTaxPrice()
+    {
+        return Price + Price * GST / 100;
+    }
+
+    public float GetDiscountPrice()
+    {
+        float taxPrice = GetTaxPrice();
+        return taxPrice - taxPrice * Discount / 100;
+    }
+
+    public float GetTotalPrice()
+    {
+        return GetDiscountPrice() * Stock;
+    }
+
+    public string Display()
+    {
+        string formattedString = $"Product ID: {ProductID}\n" +
+                                 $"Name: {Name}\n" +
+                                 $"Manufacturing Date: {ManufacturingDate}\n" +
+                                 $"Warranty (months): {warrantyMonths}\n" +
+                                 $"Tax Price: {GetTaxPrice()}\n" +
+                                 $"Discount Price: {GetDiscountPrice()}\n" +
+                                 $"Total Price: {GetTotalPrice()}";
+
+        return formattedString;
+    }
+}
+
+
+class ProductManager
+{
+    static void Main()
+    {
+        Product myProduct = new Product();
+
+        while (true)
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Enter product details");
+            Console.WriteLine("2. Display product details");
+            Console.WriteLine("3. Exit");
+
+            int choice;
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter product details:");
+
+                        Console.Write("Product ID: ");
+                        myProduct.ProductID = int.Parse(Console.ReadLine());
+
+                        Console.Write("Name: ");
+                        myProduct.Name = Console.ReadLine();
+
+                        Console.Write("Manufacturing Date (yyyy-MM-dd): ");
+                        myProduct.ManufacturingDate = Console.ReadLine();
+
+                        Console.Write("Warranty: ");
+                        myProduct.Warranty = int.Parse(Console.ReadLine());
+
+                        Console.Write("Price: ");
+                        myProduct.Price = float.Parse(Console.ReadLine());
+
+                        Console.Write("Stock: ");
+                        myProduct.Stock = int.Parse(Console.ReadLine());
+
+                        Console.Write("GST (5, 12, 18, or 28): ");
+                        myProduct.GST = int.Parse(Console.ReadLine());
+
+                        Console.Write("Discount (1-30): ");
+                        myProduct.Discount = int.Parse(Console.ReadLine());
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine(myProduct.Display());
+                        break;
+
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            Console.WriteLine("\nPress Enter to continue...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
---
+++ Day 7 Files/Notes.md
@@ -0,0 +1,38 @@
+# Day 7 Notes
+
+Instructor Email: anilkbs23@gmail.com
+
+Shared Drive [Link](https://drive.google.com/drive/folders/1DAw08ustFbkLb9CX9wGQkDZL5Ckseg1k)
+
+### Download These
+- Visual Studio 2022 Community Edition
+    - Desktop Development Workload
+
+### .Net Fullstack Development
+- Made by Microsoft
+- Install Visual Studio and Sign In with Microsoft Account
+- .NET 4.8 will be used. .Net uses C#.
+- ![Imgur](https://i.imgur.com/fu5VoBn.png)
+
+### C#
+- There's two kinds of datatypes in C#
+    - ValueType: Stored directly in Stack. Structures go here.
+    - ReferenceType: Stack contains reference to data. Actual data lives somewhere else. Classes go here.
+    - C# does not allow working with pointers. But it does contain pointers.
+
+- Classes can access members of other classes within their package depending on the access modifiers specified.
+    - private, protected, internal, private and internal protected
+
+
+**Do not print stuff directly from inside class objects. Use getter methods and print from either a printer class or the main method**
+
+### Garbage Collection
+
+
+### Assignment
+- Product Class (id, name, mfgdate, warranty, price, stock, gst, discount) *product.cs*
+    - display method : string 
+        - price
+        - taxprice
+        - discount price
+- *Program.cs*: Check screenshot
